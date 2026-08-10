2223b2b66aac9fec2c08fb8da110954a09c6db4c
YemenLinux <bash@yemenmobile.com.ye>
2026-08-10 18:48:05 +0300

    Fix requirements for moviepy and cuda13.0

--- scripts/test_experiment.py
+++ scripts/test_experiment.py
@@ -3,8 +3,9 @@ import sys
 import os
 from pathlib import Path
 
-# Add project root to path (adjust if notebook is in a subfolder)
-sys.path.insert(0, str(Path(__file__).parent.parent ))
+project_root = Path(__file__).parent.parent
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
 #
 import warnings
 # Suppress the specific future warning from torchrl
@@ -50,11 +51,12 @@ from urbanmarl.callback.evaluate_los_per_urban import EvaluateLoS
 
 if __name__ == "__main__":
     # main()
-
+    
     # Loads from "../conf/experiment/base_experiment.yaml"
     experiment_config = ExperimentConfig.get_from_yaml()
     # configure absolute save_folder to ../results/ for easier access
-    experiment_config.save_folder = Path(__file__).parent.parent /"outputs/test"
+    output_dir = project_root / "outputs" / "test"
+    experiment_config.save_folder = output_dir
     os.makedirs(experiment_config.save_folder, exist_ok=True)
     if torch.cuda.is_available():
         experiment_config.device = "cuda"
@@ -63,6 +65,7 @@ if __name__ == "__main__":
         # experiment_config.buffer_device = "cuda"
     experiment_config.max_n_iters = 3
     experiment_config.parallel_collection = True
+    experiment_config.checkpoint_at_end = True
     # experiment_config.off_policy_use_prioritized_replay_buffer = True
     # Initialize targeted operational context mapping
     # task = UrbanEnvTask.UAVMEC_OFFLOADING.get_from_yaml()
@@ -87,6 +90,5 @@ if __name__ == "__main__":
         config=experiment_config,
         callbacks=[EvaluateLoS()],
     )
-    experiment.name = f"{experiment.algorithm_name}"
-    experiment.folder_name = f"{experiment.algorithm_name}_{experiment.task_name}"
+    #
     experiment.run()
